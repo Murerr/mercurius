778313aed402c39118918d9111ef0cebf4dd60b3
murerr <rudy.murer.pro@gmail.com>
2021-02-06 18:36:26 +0100

    Adding categories and changing UI colors

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,12 +50,21 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
+        light: {
+          primary: '#3949AB',
+          accent: '#B71C1C',
+          secondary: '#00227b',
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        },
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
+          primary: '#3949AB',
+          accent: '#EF4135',
+          secondary: '#EF4135',
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
